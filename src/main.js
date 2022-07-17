1984a0709e79230eb1f04a20dbfb46e02fd47bf3
liujun <18316880540@163.com>
2022-07-17 11:02:51 +0800

    ✨feature: 初始化工程

---
+++ src/main.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: liujun 18316880540@163.com
+ * @Date: 2022-07-16 18:16:27
+ * @LastEditors: liujun 18316880540@163.com
+ * @LastEditTime: 2022-07-17 10:50:21
+ * @FilePath: \fileUploade:\WWW\vue-admin-template\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+// set ElementUI lang to EN
+// Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: (h) => h(App)
+})
